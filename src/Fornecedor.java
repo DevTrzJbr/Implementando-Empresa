7fed67f232a1332417c0bbdf8d5f5ec26b5b4eb7
Joao Victor Antunes <joao.extremo722@gmail.com>
2021-11-19 20:43:23 -0300

    Class Fornecedor

---
+++ src/Fornecedor.java
@@ -0,0 +1,27 @@
+public class Fornecedor extends Pessoa {
+    private float plafond;
+    private float valorEmDivida;    
+
+    public Fornecedor(String nome, long contribuinte, int idade, float plafond, float valorEmDivida) {
+        super(nome, contribuinte, idade);
+        this.plafond = plafond;
+        this.valorEmDivida = valorEmDivida;
+    }
+
+    public float getPlafond() {
+        return this.plafond;
+    }
+
+    public void setPlafond(float plafond) {
+        this.plafond = plafond;
+    }
+
+    public float getValorEmDivida() {
+        return this.valorEmDivida;
+    }
+
+    public void setValorEmDivida(float valorEmDivida) {
+        this.valorEmDivida = valorEmDivida;
+    }
+
+}
