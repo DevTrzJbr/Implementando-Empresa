29fb93385e925bc749e2402d37823180d34281f1
Joao Victor Antunes <joao.extremo722@gmail.com>
2021-11-19 14:40:27 -0300

    Class Produto

--- src/Produto.java
+++ src/Produto.java
@@ -1,5 +1,36 @@
 
 
 public class Produto {
+    String designacao;
+    float precoVendaPublico;
+    int stock;
+
+    public void mostrar() {
+
+    }
+
+    public String getDesignacao() {
+        return this.designacao;
+    }
+
+    public void setDesignacao(String designacao) {
+        this.designacao = designacao;
+    }
+
+    public float getPrecoVendaPublico() {
+        return this.precoVendaPublico;
+    }
+
+    public void setPrecoVendaPublico(float precoVendaPublico) {
+        this.precoVendaPublico = precoVendaPublico;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 
 }
