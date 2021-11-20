36049b7d4fc6ecb175851c5fc394fd12aff26328
Joao Victor Antunes <joao.extremo722@gmail.com>
2021-11-19 21:20:03 -0300

    Class Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,41 @@
+public class Cliente extends Pessoa {
+    private float plafond; // teto (limite crédito)
+    private float valorEmDivida;
+    private float saldo;
+    
+    public Cliente(String nome, long contribuinte, int idade, float plafond, float valorEmDivida) {
+        super(nome, contribuinte, idade);
+        this.plafond = plafond;
+        this.valorEmDivida = valorEmDivida;
+    }
+    
+    public float obterSaldo() {
+        return this.saldo = this.plafond - this.valorEmDivida;
+    }
+
+    public float getPlafond() {
+        return this.plafond;
+    }
+
+    public void setPlafond(float plafond) {
+        this.plafond = plafond;
+    }
+
+    public float getValorEmDivida() {
+        return this.valorEmDivida;
+    }
+
+    public void setValorEmDivida(float valorEmDivida) {
+        this.valorEmDivida = valorEmDivida;
+    }
+
+    public float getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+
+}
