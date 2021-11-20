c88e8d8b15573ea0503007157872cb99ce85ba78
Joao Victor Antunes <joao.extremo722@gmail.com>
2021-11-19 21:57:30 -0300

    calcularSalario() add

--- src/Empregado.java
+++ src/Empregado.java
@@ -11,8 +11,26 @@ public class Empregado extends Pessoa {
         this.iRS = iRS;
     }
 
+    // fonte: https://info.portaldasfinancas.gov.pt/pt/informacao_fiscal/codigos_tributarios/cirs_rep/Pages/irs68.aspx
     public float calcularSalario() {
-        return this.salario = this.salarioBase * this.iRS;
+
+        if (salarioBase >= 7112) {
+            iRS = 0.145f;
+        } else if (salarioBase >= 10732) {
+            iRS = 0.23f;
+        } else if (salarioBase >= 20322) {
+            iRS = 0.28f;
+        } else if (salarioBase >= 25075) {
+            iRS = 0.35f;
+        } else if (salarioBase >= 36967) {
+            iRS = 0.37f;
+        } else if (salarioBase >= 80882) {
+            iRS = 0.45f;
+        } else {
+            iRS = 0.48f;
+        }
+
+        return this.salario = this.salarioBase * 1 - this.iRS;
     }
     
     public int getNumeroSeccao() {
