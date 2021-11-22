03183d2dc1f80266b8d79f2d22982dc5c75f8f31
Joao Victor Antunes <joao.extremo722@gmail.com>
2021-11-22 15:57:42 -0300

    add: testes; MostraFornecedor

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        Empresa c1 = new Empresa();
+
+        c1.fornecedor();
+        c1.mostraFornecedores();
+    }   
+}
--- src/Empresa.java
+++ src/Empresa.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Empresa {
 
     private String nome;
@@ -45,6 +48,25 @@ public class Empresa {
         
     }
     
+    public void mostraFornecedores() {
+        System.out.println("---------LISTA DE FORNECEDORES---------");
+
+        if (fornecedores.isEmpty()) {
+            System.out.println("Lista vazia!");
+        } else {
+            for (Fornecedor listaFornecedor : fornecedores) {
+                System.out.println(listaFornecedor.getNome());
+            }
+        }
+    }
+    // Testes
+    private List<Fornecedor> fornecedores = new ArrayList<>();
+    
+    public void fornecedor() {
+        fornecedores.add(new Fornecedor("João", 12312312300L, 19, 20000, 800));
+        fornecedores.add(new Fornecedor("Yuri", 32132132101L, 19, 50000, 2000));
+        fornecedores.add(new Fornecedor("Pedro", 13213213200L, 18, 30000, 1500));
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
